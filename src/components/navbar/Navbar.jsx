18d8f68beda4ee00c44cbafe551e2f1a121ff949
apurva0411 <twinklestar039@gmail.com>
2021-01-27 13:12:20 +0530

    initial

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Button } from '../Button/Button';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.jpg';
+import './Navbar.css';
+
+const Navbar = () => {
+  const {click, setClick} = useState(false)
+
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false);
+  
+    return (
+       <>
+         <nav className='navbar'>
+           <Link to='/' className='navbar-logo'>
+             <img src={ logo } alt='logo'></img>
+           </Link>
+           <div className='menu-icon' onclick={handleClick}>
+             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+           </div>
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+             <li className='nav-item'>
+               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                 Home
+               </Link>
+             </li>
+             <li className='nav-item'>
+               <Link to='/aboutUs' className='nav-links' onClick={closeMobileMenu}>
+                 about us
+               </Link>
+             </li>
+             <li className='nav-item'>
+               <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                 Contact Us
+               </Link>
+             </li>
+             <li className='nav-item'>
+               <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                 Sign Up
+               </Link>
+             </li>
+           </ul>
+           <Button />
+         </nav>
+       </>
+    )
+}
+
+export default Navbar
